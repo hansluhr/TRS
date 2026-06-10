464dd7ad4fd4dbee2cd75cb5f5c7ae7230b2740d
hansluhr <gablins@yahoo.com.br>
2026-06-10 18:01:31 -0300

    rename

--- Rotinas/extracao_sia_basedados.R
+++ Rotinas/extracao_sia_basedados.R
@@ -5,6 +5,21 @@ set_billing_id("juntarcnpj")
 #Declaração pasta raiz.
 here::i_am("Rotinas/extracao_sia.R")
 
+#### 03.05.01.001-8 - DIÁLISE PERITONEAL INTERMITENTE DPI (1 SESSÃO POR SEMANA -EXCEPCIONALIDADE)
+
+sia <- read_sql("SELECT *
+FROM `basedosdados.br_ms_sia.producao_ambulatorial`
+WHERE instrumento_registro = 'S' AND id_processamento_ambulatorial = '0305010018' ")
+
+
+
+
+
+
+
+
+
+
 
 sia <- read_sql("
 SELECT 
@@ -22,7 +37,7 @@ SELECT
     cid_secundario_categoria,
     cid_secundario_subcategoria
 FROM `basedosdados.br_ms_sia.producao_ambulatorial`
-WHERE ano_realizacao_procedimento = 2012
+WHERE ano_realizacao_procedimento BETWEEN 2012 AND 2022
   AND id_processamento_ambulatorial IN ( 
       '0305010018',
       '0305010026',
@@ -58,7 +73,32 @@ read_sql("SELECT carater_atendimento
 
 
 
-
+sia <- read_sql("
+SELECT 
+    id_municipio,
+    ano_realizacao_procedimento,
+    sexo_paciente,
+    idade_paciente,
+    raca_cor_paciente,
+    id_processamento_ambulatorial,
+    valor_produzido_procedimento,
+    quantidade_produzida_procedimento,
+    cid_principal_categoria,
+    cid_principal_subcategoria,
+    cid_secundario_categoria,
+    cid_secundario_subcategoria
+FROM `basedosdados.br_ms_sia.producao_ambulatorial`
+WHERE ano_realizacao_procedimento BETWEEN 2012 AND 2022
+  AND id_processamento_ambulatorial IN (
+      '0305010018',
+      '0305010026',
+      '0305010093',
+      '0305010107',
+      '0305010115',
+      '0305010123',
+      '0305010166',
+      '0305010182',
+      '0305020056') ")
 
 
 
