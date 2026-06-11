0a394ac837df79fcd220fe227fb3d48f7edb4926
hansluhr <gablins@yahoo.com.br>
2026-06-11 09:57:24 -0300

    03.05.01.002-6

--- Rotinas/extracao_sia_debeaver.R
+++ Rotinas/extracao_sia_debeaver.R
@@ -1,6 +1,6 @@
 
 
-
+# Conexão -----------------------------------------------------------------
 library("RPostgreSQL")
 
 drv = dbDriver("PostgreSQL")
@@ -11,6 +11,51 @@ con = dbConnect(drv, dbname = "sis_datasus",
 
 #Check de tabelas disponíveis
 sort(dbListTables(con))
+
+
+
+# 03.05.01.001-8 - DIÁLISE PERITONEAL INTERMITENTE DPI (1 SESSÃO P --------
+qrySim = paste0("SELECT *
+FROM public.sia_br_pa 
+WHERE pa_docorig = 'S' AND pa_proc_id = '0305010018' ")
+
+t0=Sys.time()
+sia=dbGetQuery(con,qrySim)
+t1=Sys.time()
+duracao1 = t1-t0
+duracao1
+
+sia |>
+  rio::export(x = _,
+              "dialise_secund.xlsx")
+
+
+
+# 03.05.01.002-6 - DIÁLISE PERITONEAL INTERMITENTE DPI (MÁXIMO 2 Sessões por semana --------
+qrySim = paste0("SELECT *
+FROM public.sia_br_pa 
+WHERE pa_docorig = 'P' AND pa_proc_id = '0305010026' ")
+
+t0=Sys.time()
+sia=dbGetQuery(con,qrySim)
+t1=Sys.time()
+duracao1 = t1-t0
+duracao1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
